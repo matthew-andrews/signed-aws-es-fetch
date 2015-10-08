bf1d0ca318d010eba4cd01e933bc0c3f14f4f516
Matthew Andrews <matt@mattandre.ws>
2015-10-08 16:48:19 +0100

    Fix a bug and provide ways to overwrite the creds

--- main.js
+++ main.js
@@ -5,19 +5,23 @@ const fetch = global.fetch || require('node-fetch');
 const urlParse = require('url').parse;
 const resolveCname = require('denodeify')(require('dns').resolveCname);
 
-module.exports = function(url, opts) {
+module.exports = function(url, opts, creds) {
 	let urlObject = urlParse(url);
 	if (/\.es\.amazonaws\.com$/.test(urlObject.host)) {
-		return fetch(url, opts);
+		return signedFetch(url, opts, creds);
 	} else {
 		return resolveCname(urlObject.host)
 			.then(function(host) {
-				return signedFetch(url.replace(urlObject.host, host[0]), opts);
+				return signedFetch(url.replace(urlObject.host, host[0]), opts, creds);
 			});
 	}
 };
 
-function signedFetch(url, opts) {
+function signedFetch(url, opts, creds) {
+	creds = creds || {};
+	creds.accessKeyId  = creds.accessKeyId || process.env.ES_AWS_ACCESS_KEY || process.env.AWS_ACCESS_KEY;
+	creds.secretAccessKey  = creds.secretAccessKey || process.env.ES_AWS_SECRET_ACCESS_KEY || process.env.AWS_SECRET_ACCESS_KEY;
+
 	const urlObject = urlParse(url);
 	const signable = {
 		method: opts.method,
@@ -26,10 +30,7 @@ function signedFetch(url, opts) {
 		body: opts.body,
 		headers: opts.headers
 	};
-	aws4.sign(signable, {
-		accessKeyId: process.env.ES_AWS_ACCESS_KEY,
-		secretAccessKey: process.env.ES_AWS_SECRET_ACCESS_KEY
-	});
+	aws4.sign(signable, creds);
 	opts.headers = signable.headers;
 	return fetch(`${urlObject.protocol}//${opts.headers.Host}${signable.path}`, opts);
 }
